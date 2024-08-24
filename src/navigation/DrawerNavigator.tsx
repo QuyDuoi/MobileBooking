f5f4aaf733f5c6c2b95ad784f87477a8af1ed783
IMTALabs <cuongdang5124@gmail.com>
2024-08-24 21:25:24 +0700

    Code giao dien cac chuc nang quan ly dich vu chua co API

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import EmployeeManagement from '../features/EmployeeManagement/EmployeeManagement';
 import Category from '../features/EmployeeManagement/ScreenCategory/Category';
+import ServiceManagement from '../features/ScreenService/ServiceManagement';
 
 const Drawer = createDrawerNavigator();
 
@@ -13,6 +14,7 @@ function DrawerNavigator(): React.JSX.Element {
     <Drawer.Navigator initialRouteName="Category">
       <Drawer.Screen name="Home" component={EmployeeManagement} />
       <Drawer.Screen name="Category" component={Category} options={{title:'Danh mục'}}/>
+      <Drawer.Screen name="Service" component={ServiceManagement} options={{title:'Dịch Vụ'}}/>
     </Drawer.Navigator>
     </NavigationContainer>
   );
